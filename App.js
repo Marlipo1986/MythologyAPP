f0cd61d6a9497951a8640824cf7b18155ba11c5b
Martin Liporace <marlipo1986@gmail.com>
2024-06-02 13:35:29 -0300

    se agregaron hooks

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [textItem, setTextItem] = useState("");
+  const [itemList, setItemList] = useState([]);
+
+  const onHandlerChangeItem = (t) => setTextItem(t);
+
+  const add = () => {
+    setItemList((currentItems) => [
+      ...currentItems,
+      { id: Math.random().toString(), value: textItem },
+    ]);
+    setTextItem("");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Buenas</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Escriba aquí"
+          style={styles.inputContainer}
+          onChangeText={onHandlerChangeItem}
+          value={textItem}
+        />
+        <Button title="Agregar" />
+      </View>
+      <View style={styles.inputContainer}>
+        {itemList.map((item) => (
+          <View>
+            <Text>{item.value}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 60,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: 200,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
   },
 });
-
